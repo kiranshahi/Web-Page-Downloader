31132efaea553f44b74da2afca1370f721cfc972
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-03-07 11:52:16 +0545

    project added.

---
+++ Web Page Downloader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Web_Page_Downloader
+{
+    class Program
+    {
+        private readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+        }
+
+        async Task Scrapper()
+        {
+            Console.WriteLine("Enter URL: ");
+            string url = Console.ReadLine();
+            try
+            {
+                var responseString = await client.GetStringAsync(url);
+                Console.WriteLine("Enter the path to save the file. Example E:\tmp\test.txt");
+                string path = Console.ReadLine();
+                await File.WriteAllTextAsync(path, responseString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }
+    }
+}
